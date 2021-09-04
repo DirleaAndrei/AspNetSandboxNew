4a53f03c7fcb5209a7b148d4eb4a1b4ff6be4885
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-04 11:57:28 +0300

    Added Model for GetCityCoordinates method

---
+++ AspNetSandbox/WeatherForecastCityCoordinates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetSandbox
+{
+    public class WeatherForecastCityCoordinates
+    {
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+}
