afc751fd54569b66f814ed8ba6ef3c3f12eb62ba
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-22 11:42:16 +0300

    Added one argument for main

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using CommandLine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -5,15 +7,30 @@ namespace AspNetSandbox
 {
     public class Program
     {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
         public static int Main(string[] args)
         {
-            if(args.Length != 0)
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+            if (args.Length != 0)
             {
                 System.Console.WriteLine($"There are {args.Length} arguments.");
-                //foreach (arg in args)
-                //{
-                //    System.Console.WriteLine(arg);
-                //}
             }
 
             CreateHostBuilder(args).Build().Run();
