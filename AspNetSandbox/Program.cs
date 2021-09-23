f5714f915cedd3b2db0dfbffd4a9fb82de8990c1
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2021-09-23 10:46:09 +0300

    Added UsersManagement project

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -1,7 +1,19 @@
 using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using AspNetSandbox.Data;
+using AspNetSandbox.Interfaces;
+using AspNetSandbox.Services;
 using CommandLine;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Npgsql;
 
 namespace AspNetSandbox
 {
@@ -11,6 +23,12 @@ namespace AspNetSandbox
         {
             [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
             public bool Verbose { get; set; }
+
+            [Option('c', Required = false, HelpText = "Set connection string to connect to PostgresSql.")]
+            public string ConnectToPostgresDatabase { get; set; }
+
+            [Option('h', "help", Required = false, HelpText = "Help menu.")]
+            public bool Help { get; set; }
         }
 
         public static int Main(string[] args)
@@ -28,6 +46,27 @@ namespace AspNetSandbox
                            Console.WriteLine($"Current Arguments: -v {o.Verbose}");
                            Console.WriteLine("Quick Start Example!");
                        }
+
+                       if (o.Help)
+                       {
+                           //Console.WriteLine($"{o.Verbose.ToString()} {o.Verbose.}.");
+                           //Console.WriteLine("c  ConnectionString for database connection.");
+                           //Console.WriteLine("h  Help menu.");
+                       }
+                       if (o.ConnectToPostgresDatabase != "")
+                       {
+                           //var host = CreateHostBuilder(args).Build();
+
+                           //var config = host.Services.GetRequiredService<IConfiguration>();
+                           //var services = host.Services.GetRequiredService<IServiceCollection>();
+
+                           ////Console.WriteLine(config.GetConnectionString($"{o.ConnectToPostgresDatabase}"));
+                           //services.AddDbContext<ApplicationDbContext>(options =>
+                           // options.UseNpgsql(config.GetConnectionString($"{o.ConnectToPostgresDatabase}")));
+
+                           //host.Run();
+                           //TestConnectionString(o.ConnectToPostgresDatabase);
+                       }
                    });
             if (args.Length != 0)
             {
@@ -44,5 +83,14 @@ namespace AspNetSandbox
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        static void TestConnectionString(string connectionString)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            if (conn.State == System.Data.ConnectionState.Open)
+                Console.WriteLine("Success open postgreSQL connection.");
+            conn.Close();
+        }
     }
 }
---
+++ UsersManagement/Controllers/UsersControllercs.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersManagement.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        public async Task<IActionResult> Details(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Username,Email")] IdentityUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                await userManager.CreateAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(user);
+        }
+
+        public async Task<IActionResult> Edit(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Username,Email")] IdentityUser user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await userManager.UpdateAsync(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (await userManager.FindByIdAsync(id) != null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return Ok(user);
+        }
+
+        public async Task<IActionResult> Delete(string? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            await userManager.DeleteAsync(user);
+
+            return Ok(user);
+        }
+    }
+}
